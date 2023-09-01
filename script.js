7ce9baecf834a396c7da0cce32c72c7b78d32a2c
Conor Dunne <conordunne@outlook.ie>
2023-09-01 16:01:22 +0900

    Add css text to index

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const pic = document.getElementById("pic");
 const rotateBtn = document.getElementById("rotate");
 const scaleBtn = document.getElementById("scale");
+const cssText = document.getElementById("cssText");
 
 
 
-rotateBtn.addEventListener("click", () => pic.className = "rotate");
-scaleBtn.addEventListener("click", () => pic.className = "scale");
+rotateBtn.addEventListener("click", () => {
+    const css = "rotate(0.1turn)"
+    pic.style.rotate = css;
+    cssText.textContent = css;
+});
+scaleBtn.addEventListener("click", () => {
+    const css = "scale(2.5, 1.0)"
+    pic.style.transform = css;
+    cssText.textContent = css;
+});
 
 
+// console.log(window.getComputedStyle(pic).cssText);
