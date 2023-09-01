43351785d5af83643da171df727a5f10765ae0ca
Conor Dunne <conordunne@outlook.ie>
2023-09-01 16:05:24 +0900

    Remove css property on click

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ const cssText = document.getElementById("cssText");
 
 rotateBtn.addEventListener("click", () => {
     const css = "rotate(0.1turn)"
-    pic.style.rotate = css;
+    pic.style.removeProperty("transform")
+    pic.style.transform = css;
     cssText.textContent = css;
 });
 scaleBtn.addEventListener("click", () => {
     const css = "scale(2.5, 1.0)"
+    pic.style.removeProperty("transform")
     pic.style.transform = css;
     cssText.textContent = css;
 });
