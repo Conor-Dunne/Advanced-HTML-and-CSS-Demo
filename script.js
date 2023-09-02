c226b4a739a7e25903534c1fe6bb52b890c64a25
Conor Dunne <conordunne@outlook.ie>
2023-09-02 17:09:00 +0900

    Add translate css

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const pic = document.getElementById("pic");
 const rotateBtn = document.getElementById("rotate");
 const scaleBtn = document.getElementById("scale");
+const skewBtn = document.getElementById("skew");
+const translateBtn = document.getElementById("translate");
 const cssText = document.getElementById("cssText");
 
 
@@ -17,6 +19,23 @@ scaleBtn.addEventListener("click", () => {
     pic.style.transform = css;
     cssText.textContent = css;
 });
+
+skewBtn.addEventListener("click", () => {
+    const css = "skew(45deg, -0.2rad)"
+    pic.style.removeProperty("transform")
+    pic.style.transform = css;
+    cssText.textContent = css;
+});
+
+translateBtn.addEventListener("click", () => {
+    const css = "translate(100px, 150px)"
+    pic.style.removeProperty("transform")
+    pic.style.transform = css;
+    cssText.textContent = css;
+});
+
+
+
 
 
 // console.log(window.getComputedStyle(pic).cssText);
